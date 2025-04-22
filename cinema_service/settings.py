3bba27f60c928e56bde5edcbed131fb52119ccd6
yevpy <eugene2piskun@gmail.com>
2025-04-23 01:29:39 +0300

    Solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
     ],
 }
